ff3dcb9ae6d9facd6c1fbbb0d7f7798ccf1247a9
Khushi Jain <khushi.j@intimetec.com>
2023-11-06 20:50:29 +0530

    CarbonFootprint

---
+++ CarbonFootPrintEvaluator/CarbonFootPrintEvaluator/Controllers/CarbonFootprintController.cs
@@ -0,0 +1,34 @@
+﻿using CarbonFootPrintEvaluator.Model;
+using CarbonFootPrintEvaluator.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CarbonFootPrintEvaluator.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CarbonFootprintController : ControllerBase
+{
+    private readonly ILogger<CarbonFootprintController> _logger;
+    private readonly ICarbonFootprintService _carbonFootprintService;
+
+    public CarbonFootprintController(ILogger<CarbonFootprintController> logger, ICarbonFootprintService carbonFootPrintService)
+    {
+        _logger = logger;
+        _carbonFootprintService = carbonFootPrintService;
+    }
+
+    [HttpPost]
+    public ActionResult<EmailCarbonFootprint> GetCarbonFootprintForEmail(EmailDataRequest emailDataRequest)
+    {
+        try
+        {
+            return Ok(_carbonFootprintService.GetCarbonFootprintForEmail(emailDataRequest));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+            return Problem(statusCode: (int)HttpStatusCode.BadRequest, title: ex.Message, type: ((int)HttpStatusCode.BadRequest).ToString());
+        }
+    }
+}
---
+++ CarbonFootPrintEvaluator/CarbonFootPrintEvaluator/Service/CarbonFootprintService.cs
@@ -0,0 +1,21 @@
+﻿using CarbonFootPrintEvaluator.Common;
+using CarbonFootPrintEvaluator.Model;
+
+namespace CarbonFootPrintEvaluator.Service;
+
+public class CarbonFootprintService : ICarbonFootprintService
+{
+
+    public EmailCarbonFootprint GetCarbonFootprintForEmail(EmailDataRequest emailDataRequest)
+    {
+
+        var CarbonFootPrintByStandardEmails = emailDataRequest.NumberOfStandardEmails * Constants.CarbonEmissionForStandardEmail;
+        var CarbonFootPrintByLongEmails = emailDataRequest.NumberOfLongEmails * Constants.CarbonEmissionForLongEmail;
+        var CarbonFootPrintBySpamEmails = emailDataRequest.NumberOfSpamEmails * Constants.CarbonEmissionForSpamEmail;
+
+        return new EmailCarbonFootprint(CarbonFootPrintByLongEmails, CarbonFootPrintByStandardEmails, CarbonFootPrintBySpamEmails)
+        {
+            EmailId = emailDataRequest.EmailId,
+        };
+    }
+}
